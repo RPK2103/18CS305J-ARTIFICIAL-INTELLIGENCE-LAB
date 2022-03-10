d7a52b6a95fb2ddfbe90448107ed46551661ac9f
Kaviyashre Ragupathy <60558428+RPK2103@users.noreply.github.com>
2022-03-10 08:19:49 +0530

    Create BASE+BALL=GAMES.py

---
+++ EXP-3/BASE+BALL=GAMES.py
@@ -0,0 +1,11 @@
+def solutions(): 
+ # letters = ('s', 'e', 'n', 'd', 'm', 'o', 'r', 'y') 
+ all_solutions = list() 
+ for s in range(9, -1, -1): 
+ for e in range(9, -1, -1): 
+ for n in range(9, -1, -1): 
+ for d in range(9, -1, -1): 
+ for m in range(9, 0, -1): 
+ for o in range(9, -1, -1): 
+ for r in range(9, -1, -1): 
+ for y in range(9, -1, -1): 
